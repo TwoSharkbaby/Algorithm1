54a1c9e1c18487c4159cfd1cc1a4b90dc9eedd61
TwoSharkbaby <wjdekdns6602@naver.com>
2023-06-03 21:51:54 +0900

    6-7단계 까지 완료

--- src/main/java/level6/step7.java
+++ src/main/java/level6/step7.java
@@ -10,12 +10,53 @@ public class step7 {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        int repetition = Integer.parseInt(st.nextToken());
+        String word = st.nextToken();
+        int count = word.length();
 
+        for (int i = 0; i < word.length(); i++) {
 
+            char wordChar = word.charAt(i);
 
-        bw.newLine();
+            if (wordChar == 'c') {
+                if (i < word.length() - 1) {
+                    if (word.charAt(i + 1) == '=' || word.charAt(i + 1) == '-') {
+                        i++;
+                        count--;
+                    }
+                }
+            } else if (wordChar == 's' || wordChar == 'z') {
+                if (i < word.length() - 1) {
+                    if (word.charAt(i + 1) == '=') {
+                        i++;
+                        count--;
+                    }
+                }
+            } else if (wordChar == 'l' || wordChar == 'n') {
+                if (i < word.length() - 1) {
+                    if (word.charAt(i + 1) == 'j') {
+                        i++;
+                        count--;
+                    }
+                }
+            } else if (wordChar == 'd') {
+                if (i < word.length() - 1) {
+                    if (word.charAt(i + 1) == 'z') {
+                        if (i < word.length() - 2) {
+                            if (word.charAt(i + 2) == '=') {
+                                i += 2;
+                                count -= 2;
+                            }
+                        }
+                    } else if (word.charAt(i + 1) == '-') {
+                        i++;
+                        count--;
+                    }
+                }
+            }
 
+        }
+
+        bw.write(String.valueOf(count));
 
         bw.flush();
         bw.close();
