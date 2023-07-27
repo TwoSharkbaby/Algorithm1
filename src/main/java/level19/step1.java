8b4a9639bb948999a2afa25c3c8742948eabd964
TwoSharkbaby <wjdekdns6602@naver.com>
2023-07-27 16:26:02 +0900

    19-2단계 완료

--- src/main/java/level19/step1.java
+++ src/main/java/level19/step1.java
@@ -1,33 +1,53 @@
 package level19;
 
 import java.io.*;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class step1 {
+
+    static ArrayList<Integer> arr = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        int[] n = new int[Integer.parseInt(st.nextToken())];
-        int result = 0;
+        int n = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(bf.readLine());
-        for (int i = 0; i < n.length; i++) {
-            n[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(bf.readLine());
+            String commend = st.nextToken();
+            if (commend.equals("push")) {
+                int num = Integer.parseInt(st.nextToken());
+                compute1(commend, num);
+            } else {
+                bw.write(String.valueOf(compute2(commend)));
+                bw.newLine();
+            }
         }
-
-        Arrays.sort(n);
-
-        result = n[0] * n[n.length - 1];
-
-        bw.write(String.valueOf(result));
         bw.flush();
         bw.close();
         bf.close();
-
     }
 
+    static int compute2(String commend) {
+        if (commend.equals("top")) {
+            return arr.size() == 0 ? -1 : arr.get(arr.size() - 1);
+        } else if (commend.equals("size")) {
+            return arr.size();
+        } else if (commend.equals("empty")) {
+            return arr.size() == 0 ? 1 : 0;
+        } else {
+            int num = arr.size() == 0 ? -1 : arr.get(arr.size() - 1);
+            if (num != -1) {
+                arr.remove(arr.size() - 1);
+            }
+            return num;
+        }
+    }
+
+    static void compute1(String commend, int num) {
+        arr.add(num);
+    }
 }
---
+++ src/main/java/level19/step2.java
@@ -0,0 +1,37 @@
+package level19;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class step2 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        ArrayList<Integer> arr = new ArrayList<>();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            if (num == 0) {
+                arr.remove(arr.size() - 1);
+            } else {
+                arr.add(num);
+            }
+        }
+
+        for (int num : arr) {
+            sum += num;
+        }
+
+        bw.write(String.valueOf(sum));
+        bw.flush();
+        bw.close();
+        bf.close();
+    }
+}
