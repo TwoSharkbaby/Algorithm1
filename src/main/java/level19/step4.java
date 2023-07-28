34ecc8cc522c15395157e02dacb51e9d974b7a45
TwoSharkbaby <wjdekdns6602@naver.com>
2023-07-28 14:22:34 +0900

    19-4단계 완료

---
+++ src/main/java/level19/step4.java
@@ -0,0 +1,55 @@
+package level19;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class step4 {
+
+    static ArrayList<Integer> arr = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        while (true) {
+            String words = bf.readLine();
+            if (words.equals(".")) {
+                break;
+            }
+            bw.write(compute(words));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+        bf.close();
+    }
+
+    static String compute(String words) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < words.length(); i++) {
+            char c = words.charAt(i);
+            if (c == '(' || c == '[') {
+                stack.push(c);
+            } else if (c == ')') {
+                if (stack.empty() || stack.peek() != '(') {
+                    return "no";
+                } else {
+                    stack.pop();
+                }
+            } else if (c == ']') {
+                if (stack.empty() || stack.peek() != '[') {
+                    return "no";
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        if (stack.empty()) {
+            return "yes";
+        } else {
+            return "no";
+        }
+    }
+
+}
