ae17b20e06e68137724b9133a5723f405e3bb761
TwoSharkbaby <wjdekdns6602@naver.com>
2023-08-04 13:49:57 +0900

    21-4단계 완료

---
+++ src/main/java/level21/step3.java
@@ -0,0 +1,78 @@
+package level21;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class step3 {
+
+    static int[] A, tmp;
+    static int count = 0;
+    static int result = -1;
+    static int K;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        tmp = new int[N];
+        merge_sort(A, 0, N - 1);
+        System.out.println(result);
+
+    }
+
+    public static void merge_sort(int[] A, int p, int r) {
+        if (count > K) return;
+        if (p < r) {
+            int q = (p + r) / 2;
+            merge_sort(A, p, q);
+            merge_sort(A, q + 1, r);
+            merge(A, p, q, r);
+        }
+    }
+
+    public static void merge(int[] A, int p, int q, int r) {
+        int i = p;
+        int j = q + 1;
+        int t = 0;
+
+        while (i <= q && j <= r) {
+            if (A[i] <= A[j]) {
+                tmp[t] = A[i];
+                i++;
+            } else {
+                tmp[t] = A[j];
+                j++;
+            }
+            t++;
+        }
+
+        while (i <= q)
+            tmp[t++] = A[i++];
+
+        while (j <= r)
+            tmp[t++] = A[j++];
+
+        i = p;
+        t = 0;
+        while (i <= r) {
+            count++;
+            if (count == K) {
+                result = tmp[t];
+                break;
+            }
+            A[i++] = tmp[t++];
+        }
+    }
+
+}
---
+++ src/main/java/level21/step4.java
@@ -0,0 +1,27 @@
+package level21;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class step4 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        long n = Integer.parseInt(st.nextToken());
+
+        System.out.println(compute(n));
+        bf.close();
+    }
+
+    private static long compute(long n) {
+        if (n <= 0){
+            return 1;
+        } else {
+            return n * compute(n-1);
+        }
+    }
+}
